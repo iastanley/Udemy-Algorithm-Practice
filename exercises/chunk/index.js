1f7fcd57ff49829520b2371f0f3fb62c47d9c1f6
iastanley <iasvks.craigslist@gmail.com>
2018-12-27 19:36:14 -0800

    solved chunk challenge

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,8 +8,29 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+// 1st solution
+// function chunk(array, size) {
+//   const finalArray = []; // holds chunked version of array
+//   for (let el of array) {
+//     // check last chunk in finalArray
+//     const lastChunk = finalArray[finalArray.length - 1];
+//     if (!lastChunk || lastChunk.length >= size) {
+//       finalArray.push([el]);
+//     } else {
+//       lastChunk.push(el);
+//     }
+//   }
+
+//   return finalArray;
+// }
+
+// 2nd solution - using array.slice method
 function chunk(array, size) {
-  
+  const finalArray = [];
+  for (let i = 0; i < array.length; i = i + size) {
+    finalArray.push(array.slice(i, i + size));
+  }
+  return finalArray;
 }
 
 module.exports = chunk;
