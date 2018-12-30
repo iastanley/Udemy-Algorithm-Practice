3e1a42441db14520142fb4ebac723eaa90d522d4
iastanley <iasvks.craigslist@gmail.com>
2018-12-29 21:30:53 -0800

    capitalize refactor

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,20 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  // split string into words array
+  const wordArray = str.split(' ');
+  // capitalize first letter of each word in array
+  // wordArray.forEach((word, index) => {
+  //   wordArray[index] = word[0].toUpperCase() + word.slice(1);
+  // });
+
+  // using for of loop and Array.entries to access index
+  for (let [index, word] of wordArray.entries()) {
+    wordArray[index] = word[0].toUpperCase() + word.slice(1);
+  }
+  // join words back together into string and return
+  return wordArray.join(' ');
+}
 
 module.exports = capitalize;
