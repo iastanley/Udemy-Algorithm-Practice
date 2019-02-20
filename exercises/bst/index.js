d45520b2da2d3b303cb37c174b2d63a6797bbf0d
iastanley <iasvks.craigslist@gmail.com>
2019-02-19 21:47:04 -0800

    finished bst exercise

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,51 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    //base case if there is no left or right at the correct spot then create the new node and assign to this.left or this.right
+
+    // check if data is less than this.data - explore left branch
+    if (data < this.data) {
+      // if there is a left branch we need to recursively call insert on the left node
+      if(this.left) {
+        this.left.insert(data);
+      } else {
+        this.left = new Node(data);
+      }
+    // check if data is greater than this.data - explore right branch
+    } else {
+      if(this.right) {
+        this.right.insert(data);
+      } else {
+        this.right = new Node(data);
+      }
+    }
+  }
+
+  contains(data) {
+    // base case
+    if (data === this.data) {
+      return this;
+    }
+
+    if (data < this.data && this.left) {
+      return this.left.contains(data);
+    }
+
+    if (data > this.data && this.right) {
+      return this.right.contains(data);
+    }
+
+    // if there is no this.right or this.left and data wasn't found return null
+    return null;
+  }
+}
 
 module.exports = Node;
