7b887a717c8c91f710f6bd02f86fdb533da0f366
iastanley <iasvks.craigslist@gmail.com>
2019-03-01 00:44:50 -0800

    finished validate bst exercise

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,24 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  // base case
+  // checks validity of the current node
+  if ((min && node.data < min) || (max && node.data > max)) {
+    return false;
+  }
+  // left branch
+  // checks validity of the left branch with respect to current node
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false;
+  }
+  // right branch
+  // checks validity of the right branch with respect to current node
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false;
+  }
+  // if everything passes return true
+  return true;
+}
 
 module.exports = validate;
